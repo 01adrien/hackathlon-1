c90f608abd6773b004c5c326a9139e7fd0182383
TIMO NICOLAUX <timo.nicolaux@gmail.com>
2022-05-12 15:33:57 +0200

    linked popup and selectbar to the same page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import { Popup } from '../components/Popup';
+import React, { useState } from 'react';
 import "../styles/home.css";
 
 export const Home = () => {
+  const [show, setShow] = useState(true);
+
+    function handleClick() {
+        setShow(!show)
+    }
   return (
     <div>
-      <Popup />
-      <div className='category-box'>
+      <div className={show ? 'popup' : 'popup-hide'}>
+        <p className='popup-text'>Bienvenue sur JeTree, 
+l’appli qui t’aide à repérer la borne de collecte la plus proche de chez toi.</p>
+        <button className='popup-button' onClick={handleClick}>Trouver une borne</button>
+      </div>
+      <div className={!show ? 'category-box' : 'category-box-hide'}>
         <h1 className='category-title'>Je veux jeter</h1>
       <select className='category-select'>
         <option value="Verre">Verre</option>
