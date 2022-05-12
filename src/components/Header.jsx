d576fe16cbbafb68ea4b0cd7e03537fda1df4923
Anna Boval <anna.bo1106@gmail.com>
2022-05-12 14:25:28 +0200

    navbar in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import '../styles/header.css';
+import { Link } from 'react-router-dom';
+import styles from '../styles/header.module.css';
 import '../styles/footer.css';
+import jetree from '../assets/jetree.png';
 
 export const Header = () => {
   return (
-    <div className='header'></div>
-    
-  )
-}
+    <div className="Navbar">
+      <nav>
+        <Link to="/" className={styles.logohome}>
+          <img src={jetree} className={styles.logotree} alt="my-logo" />
+        </Link>
+      </nav>
+    </div>
+  );
+};
--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Result } from '../pages/Result';
 
 export const Main = () => {
   return (
-    <div className='main'>
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/result" element={<Result />} />
-    </Routes>
+    <div className="main">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
