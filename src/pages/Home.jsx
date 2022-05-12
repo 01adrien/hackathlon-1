4171f94ab23085dd527fc494ce540a39b3bccbe9
TIMO NICOLAUX <timo.nicolaux@gmail.com>
2022-05-12 15:02:20 +0200

    created popup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Popup } from '../components/Popup';
 import "../styles/home.css";
 
 export const Home = () => {
   return (
     <div>
+      <Popup />
       <div className='category-box'>
         <h1 className='category-title'>Je veux jeter</h1>
       <select className='category-select'>
