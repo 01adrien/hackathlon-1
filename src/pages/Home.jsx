4110c2526ea7b89fda9847a76d5eff64bac76bfc
TIMO NICOLAUX <timo.nicolaux@gmail.com>
2022-05-12 14:12:53 +0200

    created select-bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import "../styles/home.css";
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className='category-box'>
+        <h1 className='category-title'>Je veux jeter</h1>
+      <select className='category-select'>
+        <option value="Verre">Verre</option>
+        <option value="Décheterie">Décheterie</option>
+        <option value="Vêtements">Vêtements</option>
+        <option value="Compost">Compost</option>
+        </select>
+      </div>
+    </div>
   )
 }
