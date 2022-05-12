8a7a21536bc61008800981116bc97215c3d32821
Félix <fb.felixberger@gmail.com>
2022-05-12 17:07:25 +0200

    change map

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,10 +30,10 @@ l’appli qui t’aide à repérer la borne de collecte la plus proche de chez t
         <h1 className='category-title'>Je veux jeter</h1>
       <select className='category-select' onChange={(e) => handleSelectCategory(e.target.value)}>
         <option value=''>Choix</option>
-        <option value='Verre'>Verre</option>
-        <option value='Dechet'>Décheterie</option>
-        <option value='vetement'>Vêtements</option>
-        <option value='compost'>Composte</option>
+        <option value='verre'>Verre</option>
+        <option value='dechetterie'>Décheterie</option>
+        <option value='vetements'>Vêtements</option>
+        <option value='compost'>Compost</option>
         </select>
       </div>
     </div>
